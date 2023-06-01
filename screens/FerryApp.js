4d37904de5f92d5add90164d478075cd6aa89e51
Tristan Thorburn <tristanthorburn@gmail.com>
2023-06-01 16:35:04 -0400

    Google Sign in Finally!

--- screens/FerryApp.js
+++ screens/FerryApp.js
@@ -1,16 +1,28 @@
-import {View, Text, StyleSheet, Pressable} from 'react-native'
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const FerryApp = (props) => {
     const { loginMethod, setLoginMethod } = props
 
     const handleLogOut = () => {
-        setLoginMethod('')
+        if(loginMethod === 'Guest'){
+            setLoginMethod('')
+        }
+        if(loginMethod.email !== ''){
+            auth()
+            .signOut()
+            .then(setLoginMethod(null));
+        }
     }
 
     return(
         <View style={styles.ferryAppContainer}>
             {loginMethod === 'Guest'
                 ? <Text>Welcome {loginMethod}</Text>
+                : null
+            }
+            {loginMethod !== 'Guest' && loginMethod.email !== null
+                ? <Text>Welcome {loginMethod.displayName}</Text>
                 : null
             }
             <Pressable onPress={handleLogOut}>
